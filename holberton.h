1ff7188c67a77e1503565d9642cc61dc977b0cfa
LhetMagnan <lmagnan17@alustudent.com>
2020-10-26 16:39:37 +0200

    printf updated

---
+++ holberton.h
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#ifndef PRINTF_HOLBERTON_H
+#define PRINTF_HOLBERTON_H
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+* struct op - flag / function object
+* @c: flag
+* @f: function
+*/
+
+typedef struct op
+{
+    char *c;
+    int (*f)(va_list);
+} op_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_c(va_list ar_list);
+int (*get_func(char s))(va_list ar_list);
+int print_s(va_list ar_list);
+int print_d(va_list ar_list);
+int print_i(va_list ar_numlist);
+int print_b(va_list binary_list);
+#endif //PRINTF_HOLBERTON_H
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "holberton.h"
+
+int main() {
+    printf("%d", _printf("lhet"));
+    return 0;
+}
